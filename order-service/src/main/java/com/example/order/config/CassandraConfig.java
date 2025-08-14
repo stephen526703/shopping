7e101b68d6d2c41998de246012ff13ac6e43bb6c
stephen526703 <zyz1307113861@gmail.com>
2025-08-14 14:04:06 -0400

    update CassandraConfig

--- order-service/src/main/java/com/example/order/config/CassandraConfig.java
+++ order-service/src/main/java/com/example/order/config/CassandraConfig.java
@@ -6,19 +6,19 @@ import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @Configuration
-@EnableCassandraRepositories(basePackages = "com.example.order.event")
+@EnableCassandraRepositories(basePackages = "com.example.order")
 public class CassandraConfig extends AbstractCassandraConfiguration {
 
-    @Value("${spring.data.cassandra.keyspace-name}")
+    @Value("${spring.cassandra.keyspace-name}")
     private String keyspace;
 
-    @Value("${spring.data.cassandra.contact-points}")
+    @Value("${spring.cassandra.contact-points}")
     private String contactPoints;
 
-    @Value("${spring.data.cassandra.port}")
+    @Value("${spring.cassandra.port}")
     private int port;
 
-    @Value("${spring.data.cassandra.local-datacenter}")
+    @Value("${spring.cassandra.local-datacenter}")
     private String datacenter;
 
     @Override
