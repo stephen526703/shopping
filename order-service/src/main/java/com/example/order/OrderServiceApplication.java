7862bf90c2077ffb50d107527336b31cc9371232
stephen526703 <zyz1307113861@gmail.com>
2025-08-13 21:48:09 -0400

    update config

--- order-service/src/main/java/com/example/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/order/OrderServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.order")
+@EnableJpaRepositories(basePackages = "com.example.order.repo")
+@EntityScan(basePackages = "com.example.order.domain")
 public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
