7213c028cf71304cc61dfa759388f2f78aa10863
stephen526703 <zyz1307113861@gmail.com>
2025-08-15 15:58:19 -0400

    remove custom CassandraConfig

--- order-service/src/main/java/com/example/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/order/OrderServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication(scanBasePackages = "com.example.order")
 @EntityScan(basePackages = "com.example.order.domain")
+@EnableCassandraRepositories(basePackages = { "com.example.order.repo", "com.example.order.event" })
 @EnableFeignClients(basePackages = "com.example.order.client")
 @EnableKafka
 
